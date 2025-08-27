33487d4c249b58b9e37ca118902c3e3461a49719
panahi1387 <148593313+panahi1387@users.noreply.github.com>
2025-08-27 11:54:13 +0330

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Miraland',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const WebViewApp(),
+    );
+  }
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: AppBar(title: Text("Miraland")),
+      body: WebView(
+        initialUrl: "https://miraland.rzb.ir", // اینجا آدرس سایتتو بذار
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
